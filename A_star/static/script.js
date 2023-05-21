ada79b5d80cd884af64defd3711867e2e26e1756
uya <uyauya9@163.com>
2023-05-22 00:12:18 +0800

    加了检测输入数字功能

--- A_star/static/script.js
+++ A_star/static/script.js
@@ -8,3 +8,38 @@ function randomGenerate() {
   startInput.value = digits.join("");
   targetInput.value = "012345678";
 }
+
+function clearStates() {
+   document.getElementById('start_state').value = '';
+   document.getElementById('target_state').value = '';
+}
+
+function validateInput() {
+        var startInput = document.getElementById("start_state");
+        var targetInput = document.getElementById("target_state");
+        var startValue = startInput.value;
+        var targetValue = targetInput.value;
+        var validPattern = /^[0-8]{9}$/;
+
+        if (!validPattern.test(startValue)) {
+          displayErrorMessage(startInput, "初始状态必须由0至8的9个数字组成");
+          return false;
+        }
+
+        if (!validPattern.test(targetValue)) {
+          displayErrorMessage(targetInput, "目标状态必须由0至8的9个数字组成");
+          return false;
+        }
+
+        return true;
+      }
+
+      function displayErrorMessage(inputElement, message) {
+        var errorElement = inputElement.parentNode.querySelector(".error-message");
+        if (!errorElement) {
+          errorElement = document.createElement("p");
+          errorElement.className = "error-message";
+          inputElement.parentNode.appendChild(errorElement);
+        }
+        errorElement.textContent = message;
+      }

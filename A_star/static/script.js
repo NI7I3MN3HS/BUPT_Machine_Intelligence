e92ca401ae3433340fe3cce3da833f29f4504d8f
alantu <alan020113@gmail.com>
2023-05-22 11:38:37 +0800

    finish

--- A_star/static/script.js
+++ A_star/static/script.js
@@ -2,45 +2,115 @@ function randomGenerate() {
   var digits = [0, 1, 2, 3, 4, 5, 6, 7, 8];
   digits.sort(() => Math.random() - 0.5);
 
-  var startInput = document.getElementById("start_state");
-  var targetInput = document.getElementById("target_state");
+  var startInputs = document
+    .getElementById("start_state")
+    .getElementsByTagName("input");
 
-  startInput.value = digits.join("");
+  for (let i = 0; i < startInputs.length; i++) {
+    startInputs[i].value = digits[i];
+  }
+
   digits.sort(() => Math.random() - 0.5);
-  targetInput.value = digits.join("");
+  var targetInputs = document
+    .getElementById("target_state")
+    .getElementsByTagName("input");
+  for (let i = 0; i < targetInputs.length; i++) {
+    targetInputs[i].value = digits[i];
+  }
 }
 
 function clearStates() {
-  document.getElementById("start_state").value = "";
-  document.getElementById("target_state").value = "";
+  var startInputs = document
+    .getElementById("start_state")
+    .getElementsByTagName("input");
+  for (let input of startInputs) {
+    input.value = "";
+  }
+  var targetInputs = document
+    .getElementById("target_state")
+    .getElementsByTagName("input");
+  for (let input of targetInputs) {
+    input.value = "";
+  }
 }
 
 function validateInput() {
-  var startInput = document.getElementById("start_state");
-  var targetInput = document.getElementById("target_state");
-  var startValue = startInput.value;
-  var targetValue = targetInput.value;
+  var startInputs = document
+    .getElementById("start_state")
+    .getElementsByTagName("input");
+  var startValue = Array.from(startInputs)
+    .map((input) => input.value)
+    .join("");
+  var targetInputs = document
+    .getElementById("target_state")
+    .getElementsByTagName("input");
+  var targetValue = Array.from(targetInputs)
+    .map((input) => input.value)
+    .join("");
   var validPattern = /^(?!.*(.).*\1)[0-8]{9}$/;
 
   if (!validPattern.test(startValue)) {
-    displayErrorMessage(startInput, "初始状态必须由0至8的9个不重复数字组成");
+    displayErrorMessage("start_state", "初始状态必须由0至8的9个不重复数字组成");
     return false;
   }
 
   if (!validPattern.test(targetValue)) {
-    displayErrorMessage(targetInput, "目标状态必须由0至8的9个不重复数字组成");
+    displayErrorMessage(
+      "target_state",
+      "目标状态必须由0至8的9个不重复数字组成"
+    );
     return false;
   }
 
   return true;
 }
 
-function displayErrorMessage(inputElement, message) {
-  var errorElement = inputElement.parentNode.querySelector(".error-message");
-  if (!errorElement) {
-    errorElement = document.createElement("p");
-    errorElement.className = "error-message";
-    inputElement.parentNode.appendChild(errorElement);
-  }
+function displayErrorMessage(stateId, message) {
+  var errorElement = document.getElementById(stateId + "_error");
   errorElement.textContent = message;
+}
+
+function prepareAndValidateInput() {
+  var startInputs = document
+    .getElementById("start_state")
+    .getElementsByTagName("input");
+  var startValue = Array.from(startInputs)
+    .map((input) => input.value)
+    .join("");
+  document.getElementById("start_state_string").value = startValue;
+
+  var targetInputs = document
+    .getElementById("target_state")
+    .getElementsByTagName("input");
+  var targetValue = Array.from(targetInputs)
+    .map((input) => input.value)
+    .join("");
+  document.getElementById("target_state_string").value = targetValue;
+
+  sessionStorage.setItem("start_state", collectInputs("start_state"));
+  sessionStorage.setItem("target_state", collectInputs("target_state"));
+
+  return validateInput();
+}
+
+// 在页面加载完成后，从 sessionStorage 中恢复用户的输入。
+window.onload = function () {
+  restoreInputs("start_state", sessionStorage.getItem("start_state"));
+  restoreInputs("target_state", sessionStorage.getItem("target_state"));
+};
+
+// 收集一个表格的输入。
+function collectInputs(tableId) {
+  var inputs = document.getElementById(tableId).getElementsByTagName("input");
+  var values = Array.from(inputs).map((input) => input.value);
+  return values.join(",");
+}
+
+// 将一个表格的输入恢复到之前的状态。
+function restoreInputs(tableId, valuesString) {
+  var inputs = document.getElementById(tableId).getElementsByTagName("input");
+  var values = valuesString.split(",");
+  for (var i = 0; i < inputs.length; i++) {
+    inputs[i].value = values[i];
+  }
 }
